706c3a40c305038a7e7a5ef7250c9bdde5761d05
Kyle Ross <kylejamesross@gmail.com>
2020-07-04 21:03:51 -0600

    Preload images and make game quicker

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -2,14 +2,14 @@ export const TICK_RATE = 3000;
 export const ICONS = ["fish", "poop", "weather"];
 export const RAIN_CHANCE = 0.2;
 export const SCENES = ["day", "raining"];
-export const DAY_LENGTH = 60;
+export const DAY_LENGTH = 30;
 export const NIGHT_LENGTH = 3;
 
 export const getNextHungerTime = (clock: number): number =>
-  Math.floor(Math.random() * 3) + 8 + clock;
+  Math.floor(Math.random() * 1) + 8 + clock;
 
 export const getNextDieTime = (clock: number): number =>
-  Math.floor(Math.random() * 2) + 5 + clock;
+  Math.floor(Math.random() * 3) + 5 + clock;
 
 export const getNextPoopTime = (clock: number): number =>
-  Math.floor(Math.random() * 3) + 8 + clock;
+  Math.floor(Math.random() * 2) + 8 + clock;
